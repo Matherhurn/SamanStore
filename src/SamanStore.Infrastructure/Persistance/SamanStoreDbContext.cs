b87767976530ac10c59522c915bbdc791aed2ca5
SamanSerkani <sam.serkani@gmail.com>
2024-10-15 16:49:30 +0330

    Create Persistance/Context ... Set Connection String in AppSetting.Json / Create Extention Method For Clean Program Class /And set Pakage as You Need

---
+++ src/SamanStore.Infrastructure/Persistance/SamanStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamanStore.Domain.Entities;
+
+namespace SamanStore.Infrastructure.Persistance;
+
+public class SamanStoreDbContext : DbContext
+{
+    #region Methods
+
+    #region Constructore
+    public SamanStoreDbContext(DbContextOptions<SamanStoreDbContext>options) :base(options)
+    {
+        
+    }
+    #endregion
+
+    #endregion
+
+    #region DbSets
+    DbSet<Product> Products => Set<Product>();
+    #endregion
+}
---
+++ src/SamanStore.Web/ConfigureService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamanStore.Infrastructure.Persistance;
+
+namespace SamanStore.Web
+{
+    public static class ConfigureService
+    {
+        #region Methoths
+
+        #region AddWebServiceCollection
+        public static IServiceCollection AddWebServiceCollection(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<SamanStoreDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            return builder.Services;
+        }
+        #endregion
+
+        #endregion
+
+
+    }
+}
